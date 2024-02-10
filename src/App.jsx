4485636d9660d2126be92ab2c4a74b9583c9f742
lekhireddyvineeth <lekhireddyvineeth@gmail.com>
2024-02-10 16:33:31 +0000

    Making the Items visible

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Fragment } from 'react';
 
 import Header from "./Components/Layout/Header";
+import Meals from './Components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
-      <Header />
-    </Fragment>
+    <Header />
+    <main>
+      <Meals />
+    </main>
+  </Fragment>
   );
 }
 
